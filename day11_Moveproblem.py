05ba3567e38e0c30d9446cacfd6512425f5ef92c
Harnoorkaur-23 <hkwaraich23@gmail.com>
2026-09-13 07:44:10 +0530

    Implement function to move zeroes in array

---
+++ day11_Moveproblem.py
@@ -0,0 +1,13 @@
+def moveZeroes(nums):
+    insert_pos = 0
+    
+    # Shuffle non-zero elements to the front
+    for num in nums:
+        if num != 0:
+            nums[insert_pos] = num
+            insert_pos += 1
+            
+    # Fill the remaining positions with zeroes
+    while insert_pos < len(nums):
+        nums[insert_pos] = 0
+        insert_pos += 1
